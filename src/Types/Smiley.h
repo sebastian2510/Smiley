cffb80a5455177ad472160f4ec10bdcb460f159f
Momo Amer <maea@it-kartellet.dk>
2025-05-21 09:50:56 +0200

    ez button

--- src/Types/Smiley.h
+++ src/Types/Smiley.h
@@ -2,13 +2,17 @@
 #include <ctime>
 #include "./SmileyType.h"
 #include "../Services/NTPService.h"
+#include <ezButton.h>
 class Smiley
 {
 public:
+    Smiley() : button(0) {}
     Smiley(int button_id, SmileyType type, int light_id)
-        : button_id(button_id), type(type), light_id(light_id) {}
-
+        : button_id(button_id), type(type), light_id(light_id) {
+            button = new ezButton(button_id);
+        }
     int getButtonId() const { return button_id; }
+    ezButton* getButton() const { return button; }
     SmileyType getType() const { return type; }
     int getLightId() const { return light_id; }
     struct tm* getTimestamp() const { return timestamp; }
@@ -16,6 +20,7 @@ public:
 
 private:
     int button_id;
+    ezButton* button;
     SmileyType type;
     int light_id;
     struct tm* timestamp;
--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,6 @@
 #define YELLOW_LED 5
 void registerSmileys();
 
-
 Smiley smiley[] = {
     Smiley(RED_BUTTON, SMILEY_TYPE_ANGRY, RED_LED),
     Smiley(YELLOW_BUTTON, SMILEY_TYPE_SAD, YELLOW_LED),
@@ -27,12 +26,8 @@ Smiley smiley[] = {
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting setup of AP...");
-
-  //Initialize services
   APService::setup();
   NTPService::setup();
-
-  //Register Smileys
   registerSmileys();
   
 
@@ -43,18 +38,14 @@ void setup() {
         Serial.printf("Button %d pressed, light %d ON\n", smiley.getButtonId(), smiley.getLightId());
         delay(1000); // Keep the light on for 1 second
         digitalWrite(smiley.getLightId(), LOW);
+        break;
       }
     }
   }
 
-  // registers pins used for waking up from deepsleep :) 
-  uint64_t wakeupPins = 0;
-  for (Smiley smiley : smiley) {
-    wakeupPins |= (1ULL << smiley.getButtonId());
-  }
-
-  // sets the wakeup pins and what kind of wake up they do
-  esp_sleep_enable_ext1_wakeup(wakeupPins, ESP_EXT1_WAKEUP_ANY_HIGH);  
+ for (Smiley smiley : smiley) {
+  uint64_t wakeup_pin_bitmask = (1ULL << smiley.getButtonId());
+    esp_sleep_enable_ext1_wakeup(wakeup_pin_bitmask, ESP_EXT1_WAKEUP_ANY_HIGH);  }
 
   // Enter deep sleep
   Serial.println("Entering deep sleep...");
@@ -64,6 +55,16 @@ void setup() {
 }
 
 void loop() {
+  // for (Smiley smiley : smiley) {
+  //   if (digitalRead(smiley.getButtonId()) == HIGH) {
+  //     digitalWrite(smiley.getLightId(), HIGH);
+  //     Serial.printf("Button %d pressed, light %d ON\n", smiley.getButtonId(), smiley.getLightId());
+  //     delay(1000); // Keep the light on for 1 second
+  //     digitalWrite(smiley.getLightId(), LOW);
+  //     esp_deep_sleep_start();
+      
+  //   }
+  // }
 }
 
 void registerSmileys() {
